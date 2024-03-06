31c5466320d4496882befb8501c9ba204500e61c
Serhii Symonchuk <150609839+webinsomniac@users.noreply.github.com>
2024-03-06 02:51:10 -0500

    contact fix

--- terminal_script.js
+++ terminal_script.js
@@ -45,7 +45,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 output.innerHTML += `<div>This is a simple portfolio website created using HTML, CSS, and JavaScript.</div>`;
                 break;
             case 'contact':
-                output.innerHTML += `<div>Contact me: serhiisymonchukca@gmail.com \\ +75073807910 \\ linked.in\y\symonchuk</div>`;
+                output.innerHTML += `<div>Contact me: serhiisymonchukca@gmail.com 
+                +75073807910
+                linked.in\\y\\symonchuk</div>`;
                 break;
             default:
                 output.innerHTML += `<div>Command not found: ${command}</div>`;
